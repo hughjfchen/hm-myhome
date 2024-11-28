91aaa305ead929674c30ae32cf96c96e26a084fd
Hugh JF Chen <hugh.jf.chen@gmail.com>
2024-11-28 10:26:24 +0800

    swithc to hm release 24.05 and update org/docx template

--- home.nix
+++ home.nix
@@ -5,7 +5,7 @@
   imports = [ ];
 
   # turn off the check between home-manager and nixpkgs version
-  home.enableNixpkgsReleaseCheck = false;
+  # home.enableNixpkgsReleaseCheck = false;
 
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -19,7 +19,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -35,9 +35,10 @@
     pkgs.shellcheck
     pkgs.nil
     pkgs.efm-langserver
+    pkgs.direnv
 
     # some useful nix tools
-    pkgs.nixfmt
+    pkgs.nixfmt-rfc-style
     pkgs.nix-diff
     pkgs.nix-du
     pkgs.nix-index
@@ -64,7 +65,7 @@
     # the ultimate document handling tool
     pkgs.pandoc
 
-    # for java
+    # for java, and the ultimate text-based drawing tool, plantuml
     pkgs.jre
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
